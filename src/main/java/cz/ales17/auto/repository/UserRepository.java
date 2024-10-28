135a8f482d3d1f60a6d53fdb7352639b930d16c1
Ales Bacul <alesbacul@gmail.com>
2024-10-28 20:39:15 +0100

    create user repository

---
+++ src/main/java/cz/ales17/auto/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package cz.ales17.auto.repository;
+
+import cz.ales17.auto.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
