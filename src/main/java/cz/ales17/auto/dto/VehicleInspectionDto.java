5b5c94fd3db1508e92e0553f2a64628805e26214
Ales Bacul <alesbacul@gmail.com>
2024-10-30 20:19:45 +0100

    create validated inspection dto

---
+++ src/main/java/cz/ales17/auto/dto/VehicleInspectionDto.java
@@ -0,0 +1,22 @@
+package cz.ales17.auto.dto;
+
+import cz.ales17.auto.entity.Car;
+import cz.ales17.auto.entity.FluidLevel;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDate;
+@Builder
+@Data
+public class VehicleInspectionDto {
+    @NotNull
+    private Long id;
+    @NotNull
+    private LocalDate inspectionDate;
+    @NotNull
+    private FluidLevel coolantLevel;
+    @NotNull
+    private boolean coolantRefilled;
+    @NotNull
+    private Car vehicle;
+}
