0bf4be838428ddcd37364a40049c71f34b9f847f
Ales Bacul <alesbacul@gmail.com>
2024-12-26 15:14:30 +0100

    add login title to auth controller

--- src/main/java/cz/ales17/auto/controller/AuthController.java
+++ src/main/java/cz/ales17/auto/controller/AuthController.java
@@ -1,12 +1,14 @@
 package cz.ales17.auto.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
     @GetMapping("/login")
-    public String loginPage() {
+    public String loginPage(Model m) {
+        m.addAttribute("title", "Přihlášení");
         return "login";
     }
 }
