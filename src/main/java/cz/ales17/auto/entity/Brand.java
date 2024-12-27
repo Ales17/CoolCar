32a3a6aba1f0f8d535873f1cb89d2a6ef6bd7dea
Ales Bacul <alesbacul@gmail.com>
2024-12-27 17:04:20 +0100

    add contructors to brand

--- src/main/java/cz/ales17/auto/entity/Brand.java
+++ src/main/java/cz/ales17/auto/entity/Brand.java
@@ -1,10 +1,14 @@
 package cz.ales17.auto.entity;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Brand extends AbstractEntity {
     public String name;
 }
