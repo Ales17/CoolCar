3d04c66736038c5234285bcc66e4b6f4d02fc27a
Ales Bacul <alesbacul@gmail.com>
2024-12-27 17:21:10 +0100

    extract principal getting to independent method

--- src/main/java/cz/ales17/auto/security/SecurityUtil.java
+++ src/main/java/cz/ales17/auto/security/SecurityUtil.java
@@ -1,27 +1,31 @@
 package cz.ales17.auto.security;
 
 import cz.ales17.auto.entity.UserEntity;
-import cz.ales17.auto.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Component
 public class SecurityUtil {
 
     public static String getSessionUsername() {
+        return Objects.requireNonNull(getPrincipal()).getUsername();
+    }
+
+
+    public static UserEntity getPrincipal() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            String currentUserName = authentication.getName();
-            return currentUserName;
+            return (UserEntity) authentication.getPrincipal();
         }
         return null;
     }
-
 
 
 }
