17ab0e3984d2beb4f9d15b3d229c1a1b6c4c559c
Ales Bacul <alesbacul@gmail.com>
2024-12-28 18:22:35 +0100

    add profile page to auth controller

--- src/main/java/cz/ales17/auto/controller/AuthController.java
+++ src/main/java/cz/ales17/auto/controller/AuthController.java
@@ -1,14 +1,39 @@
 package cz.ales17.auto.controller;
 
+import cz.ales17.auto.dto.PasswordChangeDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
+    private final UserDetailsManager userDetailsManager;
+
     @GetMapping("/login")
     public String loginPage(Model m) {
         m.addAttribute("title", "Přihlášení");
         return "login";
+    }
+
+    @GetMapping("/profile/password")
+    public String profilePage(Model m) {
+        m.addAttribute("passwordDto", new PasswordChangeDto());
+        return "profile";
+    }
+
+    @PostMapping("/profile/password")
+    public String changePassword(Model m, @ModelAttribute("passwordDto") PasswordChangeDto dto) {
+        try {
+            userDetailsManager.changePassword(dto.getOldPassword(), dto.getNewPassword());
+        } catch (Exception e) {
+            m.addAttribute("message","Hesla se neshodují");
+            return "profile";
+        }
+        return "profile";
     }
 }
