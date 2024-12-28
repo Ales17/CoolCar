84254af47dcd2dd006dff41ae4036b680ff045df
Ales Bacul <alesbacul@gmail.com>
2024-12-28 18:23:12 +0100

    add user and car list to admin controller

--- src/main/java/cz/ales17/auto/controller/AdminController.java
+++ src/main/java/cz/ales17/auto/controller/AdminController.java
@@ -1,55 +1,37 @@
 package cz.ales17.auto.controller;
 
 import cz.ales17.auto.entity.Car;
-import cz.ales17.auto.entity.Role;
 import cz.ales17.auto.entity.UserEntity;
-import cz.ales17.auto.repository.RoleRepository;
 import cz.ales17.auto.repository.UserRepository;
 import cz.ales17.auto.service.CarService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.Set;
 
 @Controller
+@Secured("ROLE_ADMIN")
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
-    @Autowired
-    private CarService carService;
+    private final CarService carService;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @GetMapping("/car")
+    @GetMapping("/cars")
     public String listCars(Model m) {
         List<Car> cars = carService.getCars();
         m.addAttribute("cars", cars);
-        return "admin";
+        return "cars-list";
     }
 
-
     @GetMapping("/users")
-    public ResponseEntity<String> listUsers(Model m) {
-        UserEntity ales = new UserEntity();
-        ales.setUsername("ales");
-        ales.setPassword(passwordEncoder.encode("password"));
-        Role admin = roleRepository.findRoleByName("ADMIN");
-        Role user = roleRepository.findRoleByName("USER");
-        ales.setRoles(Set.of(user, admin));
-        userRepository.save(ales);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public String listUsers(Model m) {
+        List<UserEntity> users = userRepository.findAll();
+        m.addAttribute("users", users);
+        return "users-list";
     }
 }
