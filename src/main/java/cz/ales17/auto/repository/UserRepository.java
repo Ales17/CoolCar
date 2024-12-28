c44659341649f8cbda49a4a7baf1de8abbe51dfc
Ales Bacul <alesbacul@gmail.com>
2024-12-28 18:22:03 +0100

    add delete by username to user repo

--- src/main/java/cz/ales17/auto/repository/UserRepository.java
+++ src/main/java/cz/ales17/auto/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByUsername(String username);
     boolean existsByUsername(String username);
+    void deleteByUsernameIs(String username);
 }
