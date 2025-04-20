0366ec0ace5048ae6f4d825a15ce2632bc15b883
Ales Bacul <alesbacul@gmail.com>
2025-04-20 15:15:58 +0200

    add profile page stub to controller

--- src/main/java/cz/ales17/auto/controller/ProfileController.java
+++ src/main/java/cz/ales17/auto/controller/ProfileController.java
@@ -13,18 +13,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class ProfileController {
     private final UserDetailsManager userDetailsManager;
+
+    @GetMapping("/profile")
+    public String profilePage(Model m) {
+        return "profile-detail";
+    }
+
     @GetMapping("/profile/password")
     public String changePasswordPage(Model m) {
         m.addAttribute("passwordDto", new PasswordChangeDto());
         return "profile";
     }
 
+
     @PostMapping("/profile/password")
     public String changePassword(Model m, @ModelAttribute("passwordDto") PasswordChangeDto dto) {
         try {
             userDetailsManager.changePassword(dto.getOldPassword(), dto.getNewPassword());
         } catch (Exception e) {
-            m.addAttribute("message","Hesla se neshodují");
+            m.addAttribute("message", "Hesla se neshodují");
             return "profile";
         }
         return "profile";
