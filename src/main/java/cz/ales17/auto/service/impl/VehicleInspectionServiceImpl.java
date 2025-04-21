4d2917471c6c10883d5261e3d8cfe944a36a9e2f
Ales Bacul <alesbacul@gmail.com>
2025-04-21 20:03:22 +0200

    fix missing impl suffix in inspection service impelementation

--- src/main/java/cz/ales17/auto/service/impl/VehicleInspectionServiceImpl.java
+++ src/main/java/cz/ales17/auto/service/impl/VehicleInspectionServiceImpl.java
@@ -4,32 +4,35 @@ import cz.ales17.auto.dto.VehicleInspectionDto;
 import cz.ales17.auto.entity.VehicleInspection;
 import cz.ales17.auto.mapper.VehicleInspectionMapper;
 import cz.ales17.auto.repository.VehicleInspectionRepository;
+import cz.ales17.auto.service.VehicleInspectionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
-public class VehicleInspectionService implements cz.ales17.auto.service.VehicleInspectionService {
+public class VehicleInspectionServiceImpl implements VehicleInspectionService {
     @Autowired
-    private VehicleInspectionRepository vehicleInspectionRepository;
+    private VehicleInspectionRepository inspectionRepository;
+
     @Override
     public List<VehicleInspection> findByCarId(Long carId) {
-        return vehicleInspectionRepository.findAllByVehicle_IdIs(carId);
+        return inspectionRepository.findAllByVehicle_IdIs(carId);
     }
 
     @Override
     public void addInspection(VehicleInspection vehicleInspection) {
-        vehicleInspectionRepository.save(vehicleInspection);
+        inspectionRepository.save(vehicleInspection);
     }
 
     @Override
     public void deleteInspectionById(Long id) {
-        vehicleInspectionRepository.deleteById(id);
+        inspectionRepository.deleteById(id);
     }
 
     @Override
     public VehicleInspectionDto findInspectionById(Long id) {
-        VehicleInspection vehicleInspection = vehicleInspectionRepository.findById(id).orElseThrow(RuntimeException::new);
+        VehicleInspection vehicleInspection = inspectionRepository.findById(id).orElseThrow(RuntimeException::new);
         return VehicleInspectionMapper.toDto(vehicleInspection);
     }
 }
