6b22a21c0d38c37fec5e0723d665857d9190776c
Ales Bacul <alesbacul@gmail.com>
2025-07-26 15:40:55 +0200

    use @Value in rest client config

--- src/main/java/cz/ales17/auto/service/RestClientConfig.java
+++ src/main/java/cz/ales17/auto/service/RestClientConfig.java
@@ -1,12 +1,14 @@
 package cz.ales17.auto.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
-    private final String API_KEY = System.getenv("API_KEY");
+    @Value("${API_KEY}")
+    private String API_KEY;
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
