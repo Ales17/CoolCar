5a5475589f078c40e8ab3c1660e23b1b07fc757d
ales_bacul <alesbacul@gmail.com>
2025-09-15 17:46:46 +0200

    create constructor in vehicle inspection dto

--- src/main/java/cz/ales17/auto/dto/VehicleInspectionDto.java
+++ src/main/java/cz/ales17/auto/dto/VehicleInspectionDto.java
@@ -3,11 +3,16 @@ package cz.ales17.auto.dto;
 import cz.ales17.auto.entity.Car;
 import cz.ales17.auto.entity.FluidLevel;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VehicleInspectionDto {
     @NotNull
     private Long id;
