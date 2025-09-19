9e5b4a978438536c8e5f0b6c0d3eef0034db77e5
ales_bacul <alesbacul@gmail.com>
2025-09-19 13:57:33 +0200

    add method for inspection owner checking

--- src/main/java/cz/ales17/auto/security/AuthorizationService.java
+++ src/main/java/cz/ales17/auto/security/AuthorizationService.java
@@ -1,7 +1,9 @@
 package cz.ales17.auto.security;
 
 import cz.ales17.auto.entity.Car;
+import cz.ales17.auto.entity.VehicleInspection;
 import cz.ales17.auto.repository.CarRepository;
+import cz.ales17.auto.repository.VehicleInspectionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class AuthorizationService {
 
     private final CarRepository carRepository;
-
+    private final VehicleInspectionRepository inspRepository;
     public boolean canSaveCar(Long carId) {
         if (carId == null) {
             return true;
@@ -25,5 +27,10 @@ public class AuthorizationService {
         return carOwnerId == authenticatedUserId;
     }
 
+    public boolean isInspectionOwner(Long inspectionId) {
+        VehicleInspection inspection = inspRepository.findById(inspectionId).orElseThrow(RuntimeException::new);
+        long inspVehicle = inspection.getVehicle().getId();
+        return isCarOwner(inspVehicle);
+    }
 
 }
