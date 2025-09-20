9dcb6c8e8ee9da1f4d466fafe2be18f0828c41eb
ales_bacul <alesbacul@gmail.com>
2025-09-20 13:21:46 +0200

    add email validation to user details dto

--- src/main/java/cz/ales17/auto/dto/UserDetailsDto.java
+++ src/main/java/cz/ales17/auto/dto/UserDetailsDto.java
@@ -1,10 +1,12 @@
 package cz.ales17.auto.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class UserDetailsDto {
     public String firstName;
     public String lastName;
+    @Email
     public String email;
 }
